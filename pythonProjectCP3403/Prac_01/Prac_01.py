376fdd5091b0c35625f735f0cdea19495f650f0b
Kazuki Pickersgill <kazuki.pickersgill@myjcu.edu.au>
2023-03-11 22:07:40 +1000

    Changes

--- pythonProjectCP3403/Prac_01/Prac_01.py
+++ pythonProjectCP3403/Prac_01/Prac_01.py
@@ -1,14 +1,15 @@
-from os.path import expanduser
-import pandas as pd
 
-# import numpy as np
 
 # Load File
+import pandas
+import pandas as pd
+
 data = pd.read_csv('mushroom.csv', header=None)
 print(data.head(20))
 
 print("Part Two")
-import pandas.api.types
+# import pandas.api.types
+# import pandas.api.types
 
 for col in data.columns:
     if not pandas.api.types.is_numeric_dtype(data[col]):
